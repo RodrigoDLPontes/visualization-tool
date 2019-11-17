e56fc4d2ce3662e47b4645c92c44566c6a73a4ec
Zachary Minot <zjminot@gmail.com>
2019-11-17 12:36:57 -0500

    QueueArray head/tail -> front/back

--- AlgorithmLibrary/QueueArray.js
+++ AlgorithmLibrary/QueueArray.js
@@ -33,15 +33,15 @@ var ARRAY_ELEM_HEIGHT = 50;
 var ARRRAY_ELEMS_PER_LINE = 15;
 var ARRAY_LINE_SPACING = 130;
 
-var HEAD_POS_X = 180;
-var HEAD_POS_Y = 100;
-var HEAD_LABEL_X = 130;
-var HEAD_LABEL_Y =  100;
+var FRONT_POS_X = 180;
+var FRONT_POS_Y = 100;
+var FRONT_LABEL_X = 130;
+var FRONT_LABEL_Y =  100;
 
-var TAIL_POS_X = 280;
-var TAIL_POS_Y = 100;
-var TAIL_LABEL_X = 230;
-var TAIL_LABEL_Y =  100;
+var BACK_POS_X = 280;
+var BACK_POS_Y = 100;
+var BACK_LABEL_X = 230;
+var BACK_LABEL_Y =  100;
 
 var QUEUE_LABEL_X = 50;
 var QUEUE_LABEL_Y = 30;
@@ -69,8 +69,8 @@ QueueArray.prototype.init = function(am, w, h)
 	this.addControls();
 	this.nextIndex = 0;
 	this.commands = [];
-	//this.tail_pos_y = h - LINKED_LIST_ELEM_HEIGHT;
-//	this.tail_label_y = this.tail_pos_y;
+	//this.back_pos_y = h - LINKED_LIST_ELEM_HEIGHT;
+//	this.back_label_y = this.back_pos_y;
 	this.setup();
 	this.initialIndex = this.nextIndex;
 }
@@ -102,9 +102,8 @@ QueueArray.prototype.enableUI = function(event)
 	{
 		this.controls[i].disabled = false;
 	}
-	
-	
 }
+
 QueueArray.prototype.disableUI = function(event)
 {
 	for (var i = 0; i < this.controls.length; i++)
@@ -127,14 +126,14 @@ QueueArray.prototype.setup = function()
 		this.arrayID[i]= this.nextIndex++;
 		this.arrayLabelID[i]= this.nextIndex++;
 	}
-	this.headID = this.nextIndex++;
-	headLabelID = this.nextIndex++;
-	this.tailID = this.nextIndex++;
-	tailLabelID = this.nextIndex++;
+	this.frontID = this.nextIndex++;
+	frontLabelID = this.nextIndex++;
+	this.backID = this.nextIndex++;
+	backLabelID = this.nextIndex++;
 	
 	this.arrayData = new Array(SIZE);
-	this.head = 0;
-	this.tail = 0;
+	this.front = 0;
+	this.back = 0;
 	this.leftoverLabelID = this.nextIndex++;
 	
 	
@@ -147,11 +146,11 @@ QueueArray.prototype.setup = function()
 		this.cmd("SetForegroundColor", this.arrayLabelID[i], INDEX_COLOR);
 		
 	}
-	this.cmd("CreateLabel", headLabelID, "Head", HEAD_LABEL_X, HEAD_LABEL_Y);
-	this.cmd("CreateRectangle", this.headID, 0, ARRAY_ELEM_WIDTH, ARRAY_ELEM_HEIGHT, HEAD_POS_X, HEAD_POS_Y);
+	this.cmd("CreateLabel", frontLabelID, "Front", FRONT_LABEL_X, FRONT_LABEL_Y);
+	this.cmd("CreateRectangle", this.frontID, 0, ARRAY_ELEM_WIDTH, ARRAY_ELEM_HEIGHT, FRONT_POS_X, FRONT_POS_Y);
 	
-	this.cmd("CreateLabel", tailLabelID, "Tail", TAIL_LABEL_X, TAIL_LABEL_Y);
-	this.cmd("CreateRectangle", this.tailID, 0, ARRAY_ELEM_WIDTH, ARRAY_ELEM_HEIGHT, TAIL_POS_X, TAIL_POS_Y);
+	this.cmd("CreateLabel", backLabelID, "Back", BACK_LABEL_X, BACK_LABEL_Y);
+	this.cmd("CreateRectangle", this.backID, 0, ARRAY_ELEM_WIDTH, ARRAY_ELEM_HEIGHT, BACK_POS_X, BACK_POS_Y);
 	
 	
 	
@@ -181,7 +180,7 @@ QueueArray.prototype.reset = function()
 		
 QueueArray.prototype.enqueueCallback = function(event)
 {
-	if ((this.tail + 1) % SIZE  != this.head && this.enqueueField.value != "")
+	if ((this.back + 1) % SIZE  != this.front && this.enqueueField.value != "")
 	{
 		var pushVal = this.enqueueField.value;
 		this.enqueueField.value = ""
@@ -192,7 +191,7 @@ QueueArray.prototype.enqueueCallback = function(event)
 		
 QueueArray.prototype.dequeueCallback = function(event)
 {
-	if (this.tail != this.head)
+	if (this.back != this.front)
 	{
 		this.implementAction(this.dequeue.bind(this), "");
 	}
@@ -212,18 +211,18 @@ QueueArray.prototype.enqueue = function(elemToEnqueue)
 	
 	var labEnqueueID = this.nextIndex++;
 	var labEnqueueValID = this.nextIndex++;
-	this.arrayData[this.tail] = elemToEnqueue;
+	this.arrayData[this.back] = elemToEnqueue;
 	this.cmd("SetText", this.leftoverLabelID, "");
 	
 	this.cmd("CreateLabel", labEnqueueID, "Enqueuing Value: ", QUEUE_LABEL_X, QUEUE_LABEL_Y);
 	this.cmd("CreateLabel", labEnqueueValID,elemToEnqueue, QUEUE_ELEMENT_X, QUEUE_ELEMENT_Y);
 	
 	this.cmd("Step");			
-	this.cmd("CreateHighlightCircle", this.highlight1ID, INDEX_COLOR,  TAIL_POS_X, TAIL_POS_Y);
+	this.cmd("CreateHighlightCircle", this.highlight1ID, INDEX_COLOR,  BACK_POS_X, BACK_POS_Y);
 	this.cmd("Step");
 	
-	var xpos = (this.tail  % ARRRAY_ELEMS_PER_LINE) * ARRAY_ELEM_WIDTH + ARRAY_START_X;
-	var ypos = Math.floor(this.tail / ARRRAY_ELEMS_PER_LINE) * ARRAY_LINE_SPACING +  ARRAY_START_Y;
+	var xpos = (this.back  % ARRRAY_ELEMS_PER_LINE) * ARRAY_ELEM_WIDTH + ARRAY_START_X;
+	var ypos = Math.floor(this.back / ARRRAY_ELEMS_PER_LINE) * ARRAY_LINE_SPACING +  ARRAY_START_Y;
 	
 	this.cmd("Move", this.highlight1ID, xpos, ypos + ARRAY_ELEM_HEIGHT); 				
 	this.cmd("Step");
@@ -231,17 +230,17 @@ QueueArray.prototype.enqueue = function(elemToEnqueue)
 	this.cmd("Move", labEnqueueValID, xpos, ypos);
 	this.cmd("Step");
 	
-	this.cmd("Settext", this.arrayID[this.tail], elemToEnqueue);
+	this.cmd("Settext", this.arrayID[this.back], elemToEnqueue);
 	this.cmd("Delete", labEnqueueValID);
 	
 	this.cmd("Delete", this.highlight1ID);
 	
-	this.cmd("SetHighlight", this.tailID, 1);
+	this.cmd("SetHighlight", this.backID, 1);
 	this.cmd("Step");
-	this.tail = (this.tail + 1) % SIZE;
-	this.cmd("SetText", this.tailID, this.tail)
+	this.back = (this.back + 1) % SIZE;
+	this.cmd("SetText", this.backID, this.back)
 	this.cmd("Step");
-	this.cmd("SetHighlight", this.tailID, 0);
+	this.cmd("SetHighlight", this.backID, 0);
 	this.cmd("Delete", labEnqueueID);
 	
 	return this.commands;
@@ -259,11 +258,11 @@ QueueArray.prototype.dequeue = function(ignored)
 	
 	this.cmd("CreateLabel", labDequeueID, "Dequeued Value: ", QUEUE_LABEL_X, QUEUE_LABEL_Y);
 	
-	this.cmd("CreateHighlightCircle", this.highlight1ID, INDEX_COLOR,  HEAD_POS_X, HEAD_POS_Y);
+	this.cmd("CreateHighlightCircle", this.highlight1ID, INDEX_COLOR,  FRONT_POS_X, FRONT_POS_Y);
 	this.cmd("Step");
 	
-	var xpos = (this.head  % ARRRAY_ELEMS_PER_LINE) * ARRAY_ELEM_WIDTH + ARRAY_START_X;
-	var ypos = Math.floor(this.head / ARRRAY_ELEMS_PER_LINE) * ARRAY_LINE_SPACING +  ARRAY_START_Y;
+	var xpos = (this.front  % ARRRAY_ELEMS_PER_LINE) * ARRAY_ELEM_WIDTH + ARRAY_START_X;
+	var ypos = Math.floor(this.front / ARRRAY_ELEMS_PER_LINE) * ARRAY_LINE_SPACING +  ARRAY_START_Y;
 	
 	this.cmd("Move", this.highlight1ID, xpos, ypos + ARRAY_ELEM_HEIGHT); 				
 	this.cmd("Step");		
@@ -271,18 +270,18 @@ QueueArray.prototype.dequeue = function(ignored)
 	this.cmd("Delete", this.highlight1ID);
 	
 	
-	var dequeuedVal = this.arrayData[this.head]
+	var dequeuedVal = this.arrayData[this.front]
 	this.cmd("CreateLabel", labDequeueValID,dequeuedVal, xpos, ypos);
-	this.cmd("Settext", this.arrayID[this.head], "");
+	this.cmd("Settext", this.arrayID[this.front], "");
 	this.cmd("Move", labDequeueValID,  QUEUE_ELEMENT_X, QUEUE_ELEMENT_Y);
 	this.cmd("Step");
 	
-	this.cmd("SetHighlight", this.headID, 1);
+	this.cmd("SetHighlight", this.frontID, 1);
 	this.cmd("Step");
-	this.head = (this.head + 1 ) % SIZE;
-	this.cmd("SetText", this.headID, this.head)
+	this.front = (this.front + 1 ) % SIZE;
+	this.cmd("SetText", this.frontID, this.front)
 	this.cmd("Step");
-	this.cmd("SetHighlight", this.headID, 0);
+	this.cmd("SetHighlight", this.frontID, 0);
 	
 	this.cmd("SetText", this.leftoverLabelID, "Dequeued Value: " + dequeuedVal);
 	
@@ -306,10 +305,10 @@ QueueArray.prototype.clearAll = function()
 	{
 		this.cmd("SetText", this.arrayID[i], "");
 	}
-	this.head = 0;
-	this.tail = 0;
-	this.cmd("SetText", this.headID, "0");
-	this.cmd("SetText", this.tailID, "0");
+	this.front = 0;
+	this.back = 0;
+	this.cmd("SetText", this.frontID, "0");
+	this.cmd("SetText", this.backID, "0");
 	return this.commands;
 	
 }

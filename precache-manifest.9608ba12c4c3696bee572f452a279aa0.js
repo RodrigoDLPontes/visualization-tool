325d28620e970335e478ae2c2018911a58bd920a
RodrigoDLPontes <rodrigo.dl.pontes@gmail.com>
2020-02-24 16:55:18 -0500

    Updates

--- precache-manifest.9608ba12c4c3696bee572f452a279aa0.js
+++ precache-manifest.9608ba12c4c3696bee572f452a279aa0.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "056d97c1cce64509e0f00ddba69a9b08",
+    "revision": "3d58ce28224bc32ce42d68c5ab87aeff",
     "url": "/index.html"
   },
   {
-    "revision": "6d79375e5d74554d40ad",
-    "url": "/static/css/main.50b252e7.chunk.css"
+    "revision": "990aee5a8920091c3f8e",
+    "url": "/static/css/main.17ba3ad6.chunk.css"
   },
   {
     "revision": "3b7c22b756e80c45d91a",
     "url": "/static/js/2.94dc897c.chunk.js"
   },
   {
-    "revision": "6d79375e5d74554d40ad",
-    "url": "/static/js/main.c7bb4d85.chunk.js"
+    "revision": "990aee5a8920091c3f8e",
+    "url": "/static/js/main.3704f9f5.chunk.js"
   },
   {
     "revision": "fd9505bb717b44967334",

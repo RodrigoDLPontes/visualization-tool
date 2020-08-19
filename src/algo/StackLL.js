bdc8ab26912f30a1796148dc3c3f2496795c1bf9
Miguel de los Reyes <mbclr3@gatech.edu>
2020-08-19 15:31:01 -0400

    Remove Unicode BOM

--- src/algo/StackLL.js
+++ src/algo/StackLL.js
@@ -1,4 +1,4 @@
-﻿// Copyright 2011 David Galles, University of San Francisco. All rights reserved.
+// Copyright 2011 David Galles, University of San Francisco. All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without modification, are
 // permitted provided that the following conditions are met:

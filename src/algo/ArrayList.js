dd123acc66e6d3b6f500ea7607550b69dd0cf307
grant-hollosi <ghollosi01@gmail.com>
2021-01-25 20:54:55 -0500

    commit

--- src/algo/ArrayList.js
+++ src/algo/ArrayList.js
@@ -199,8 +199,6 @@ export default class ArrayList extends Algorithm {
 		this.length = SIZE;
 		this.commands = [];
 
-		//Who needs this.arrayData? <- added comment so I can make a new commit for vercel
-
 		for (let i = 0; i < SIZE; i++) {
 			const xpos = (i % ARRAY_ELEMS_PER_LINE) * ARRAY_ELEM_WIDTH + ARRAY_START_X;
 			const ypos = Math.floor(i / ARRAY_ELEMS_PER_LINE) * ARRAY_LINE_SPACING + ARRAY_START_Y;

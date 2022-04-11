a956e87b1a783762432f8a856383de6f107de1ed
RodrigoDLPontes <rodrigo.dl.pontes@gmail.com>
2022-04-11 17:03:55 +0000

    Updates

--- precache-manifest.cc3879c1492112ffd84566c6f9ebb652.js
+++ precache-manifest.cc3879c1492112ffd84566c6f9ebb652.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "750c7300f622678bf6d89f074ca04b3b",
+    "revision": "49fc043db4fc0d24f9ed2ff421f32c86",
     "url": "/index.html"
   },
   {
-    "revision": "0908e5c7a8bd7f86fee3",
+    "revision": "890c2eb6853783d38294",
     "url": "/static/css/main.709db2b3.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.8cb9c7c3.chunk.js"
   },
   {
-    "revision": "0908e5c7a8bd7f86fee3",
-    "url": "/static/js/main.e96df827.chunk.js"
+    "revision": "890c2eb6853783d38294",
+    "url": "/static/js/main.6746be06.chunk.js"
   },
   {
     "revision": "fd9505bb717b44967334",

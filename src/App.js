9a312d1faaee4bb2eabee1947e87a2a7cd55e354
skapoor68 <skapoor68@gatech.edu>
2023-01-22 23:52:24 -0500

    add ThemeProvider wrapper

--- src/App.js
+++ src/App.js
@@ -4,18 +4,26 @@ import AlgoScreen from './screens/AlgoScreen';
 import HomeScreen from './screens/HomeScreen';
 import React from 'react';
 import ReactGA from 'react-ga';
+import { ThemeProvider } from 'react-hook-theme';
 
 const App = () => {
 	ReactGA.initialize('UA-164374852-1');
 	ReactGA.pageview('home');
 
 	return (
-		<Router basename={process.env.PUBLIC_URL + '/'}>
-			<Switch>
-				<Route exact path={['/', '/about']} component={HomeScreen} />
-				<Route component={AlgoScreen} />
-			</Switch>
-		</Router>
+		<ThemeProvider
+    		options={{
+        		theme: 'dark',
+        		save: true,
+    		}}
+		>
+			<Router basename={process.env.PUBLIC_URL + '/'}>
+				<Switch>
+					<Route exact path={['/', '/about']} component={HomeScreen} />
+					<Route component={AlgoScreen} />
+				</Switch>
+			</Router>
+		</ThemeProvider>
 	);
 };
 

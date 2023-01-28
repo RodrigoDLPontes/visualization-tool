96c1987d732befe953d619e654573fbc856bb6e3
skapoor68 <skapoor68@gatech.edu>
2023-01-27 21:33:27 -0500

    GT colors

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => (
 	<div className="footer">
 		&copy; Copyright 2019 <a href="https://rodrigodlpontes.github.io/website/">Rodrigo Pontes</a>,
-		under the direction of Mary Hudachek-Buswell, Copyright 2011
+		under the direction of Mary Hudachek-Buswell, &copy; Copyright 2011&nbsp;
 		<a href="http://www.cs.usfca.edu/galles">David Galles</a>
 	</div>
 );

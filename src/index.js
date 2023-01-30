6188f3a4d9b37c88f1761023bcea667f72539bde
Liam Jones <67525591+ljones315@users.noreply.github.com>
2023-01-30 11:52:00 -0500

    fix import
    gets rid of a console warning

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import App from './App';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'react-hook-theme';
 
 const root = ReactDOM.createRoot(document.getElementById("root")); 

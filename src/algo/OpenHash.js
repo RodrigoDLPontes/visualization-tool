6b9c6f4d7a4e138659817bc6d18c222085a651fa
Liam Jones <liam@Liams-MacBook-Pro.local>
2023-11-20 14:46:59 -0500

    fix probing resize issue

--- src/algo/OpenHash.js
+++ src/algo/OpenHash.js
@@ -87,7 +87,6 @@ export default class OpenHash extends Hash {
 		this.probeTypeDropDown.style.width = '132px';
 		this.probeTypeDropDown.onchange = this.checkProbeType.bind(this);
 
-
 		addDivisorToAlgorithmBar();
 
 		this.hashTypeParentGroup = addGroupToAlgorithmBar(false);
@@ -96,7 +95,11 @@ export default class OpenHash extends Hash {
 
 		this.hashTypeLabel = addLabelToAlgorithmBar('Hash Type:', this.hashTypeLabelGroup);
 		this.hashTypeDropDown = addDropDownGroupToAlgorithmBar(
-			[['Integers', 'Hash Integers'], ['Strings', 'Hash Strings'], ['True', 'True Hash Function']],
+			[
+				['Integers', 'Hash Integers'],
+				['Strings', 'Hash Strings'],
+				['True', 'True Hash Function'],
+			],
 			'Hash Type',
 			this.hashTypedropDownGroup,
 		);
@@ -179,7 +182,6 @@ export default class OpenHash extends Hash {
 		const entry = new MapEntry(key, value);
 		const elem = entry.elem;
 		this.commands = [];
-
 		if (
 			(this.size + 1) / this.table_size > this.load_factor &&
 			this.table_size * 2 + 1 < MAX_SIZE
@@ -195,6 +197,8 @@ export default class OpenHash extends Hash {
 
 		if (index === -2 && this.table_size * 2 < MAX_SIZE) {
 			this.resize(true);
+			index = this.doHash(key);
+			index = this.getEmptyIndex(index, key);
 		} else if (index === -2) {
 			this.cmd(
 				act.setText,
@@ -225,6 +229,7 @@ export default class OpenHash extends Hash {
 		this.cmd(act.move, labID, this.indexXPos[index], this.indexYPos[index] - ARRAY_ELEM_HEIGHT);
 		this.cmd(act.step);
 		this.cmd(act.delete, labID);
+		console.log(index);
 		this.cmd(act.setText, this.hashTableVisual[index], elem);
 		this.cmd(act.setHighlight, this.hashTableVisual[index], 0);
 		this.hashTableValues[index] = entry;
@@ -447,8 +452,6 @@ export default class OpenHash extends Hash {
 	}
 
 	resize(fromCycle) {
-		this.commands = [];
-
 		this.cmd(act.setText, this.ExplainLabel, '');
 		this.cmd(act.setText, this.DelIndexLabel, '');
 
